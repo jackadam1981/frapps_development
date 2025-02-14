c6415ae02d1daf5e2bc54924d06e14c9d2799013
jack <jackadam@sina.com>
2025-02-14 12:00:51 +0800

    更新build

--- build.sh
+++ build.sh
@@ -5,17 +5,15 @@ docker rm $(docker ps -a -q)
 docker volume rm $(docker volume ls -q)
 docker network rm $(docker network ls -q)
 
-rm -rf 
-rm -rf 
-rm -rf 
-rm -rf 
-
+rm -rf devcontainer
+rm -rf development
+mkdir development
+chmod 0777 development
 
 cp -R ../frappe_docker/devcontainer-example/ devcontainer
-
-wget https://gist.githubusercontent.com/guinanlin/eb8cd7534efad3234ffcdcf34b71004e/raw/cn_saas_chart_of_accounts.json -O /apps/erpnext/erpnext/accounts/doctype/account/chart_of_accounts/verified
+cp create_site.sh development/
 
 docker compose -f traefik.yaml up -d
-docker compose -f ../.devcontainer/docker-compose.yml up -d
-docker exec -it devcontainer-frappe-1 bash create_site.sh
+docker compose -f devcontainer/docker-compose.yml up -d
+docker exec -it devcontainer-frappe-1 bash
 
